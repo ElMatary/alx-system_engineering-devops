1c858eb6aeffe65a4f38b4fa5dd12b4a446b3b7c
ElMatary <mahmoudelmatary724@yahoo.com>
2024-08-16 20:41:08 +0000

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-eplace phpp to php in /var/www/html/wp-settings.php
+# Replace phpp to php in /var/www/html/wp-settings.php
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
